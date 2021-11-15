ad80ed4b42826f2c680f055a1fc98a806e142a51
AzureAD\YuvarajPalanisamy <yuvaraj.palanisamy@syncfusion.com>
2021-11-15 14:18:52 +0530

    committed

---
+++ ChartSample/Model.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ChartSample
+{
+    public class ChartModel : INotifyPropertyChanged
+    {
+        private string month;
+        public string Month 
+        {
+            get { return month; }
+            set 
+            {
+                month = value;
+                RaisePropertyChanged(nameof(Month));
+            }
+        }
+
+        public Model data;
+        public Model Data 
+        {
+            get { return data; }
+            set
+            {
+                data = value;
+                RaisePropertyChanged(nameof(Data));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void RaisePropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+    public class Model : INotifyPropertyChanged
+    {
+        private double[] yValue;
+        public double[] YValue
+        {
+            get { return yValue; }
+            set
+            {
+                if (value != yValue)
+                {
+                    yValue = value;
+                    RaisePropertyChanged(nameof(YValue));
+                }
+            }
+        }
+
+        private Product products;
+        public Product Products
+        {
+            get { return products; }
+            set
+            {
+                if (value != products)
+                {
+                    products = value;
+                    RaisePropertyChanged(nameof(Products));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void RaisePropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+    public class Product : INotifyPropertyChanged
+    {
+        private double productA;
+        public double ProductA
+        {
+            get { return productA; }
+            set
+            {
+                if (value != productA)
+                {
+                    productA = value;
+                    RaisePropertyChanged(nameof(ProductA));
+                }
+            }
+        }
+
+        private double productB;
+        public double ProductB
+        {
+            get { return productB; }
+            set
+            {
+                if (value != productB)
+                {
+                    productB = value;
+                    RaisePropertyChanged(nameof(ProductB));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void RaisePropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ ChartSample/ViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace ChartSample
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<ChartModel> categoryData;
+        public ObservableCollection<ChartModel> CategoryData
+        {
+            get { return categoryData; }
+            set
+            {
+                categoryData = value;
+                RaisePropertyChanged(nameof(CategoryData));
+            }
+        }
+
+        public ViewModel()
+        {
+            CategoryData = new ObservableCollection<ChartModel>()
+            {
+                new ChartModel()
+                {
+                    Month = "Jan",
+                    Data = new Model() { YValue = new double[] { 28, 12 }, Products = new Product(){ ProductA = 15, ProductB = 18 } }
+                },
+                
+                new ChartModel()
+                {
+                    Month = "Feb",
+                    Data = new Model() { YValue = new double[] { 18 , 20 }, Products = new Product(){ ProductA = 12, ProductB = 28 } }
+                },
+
+                new ChartModel()
+                {
+                    Month = "Mar",
+                    Data = new Model() { YValue = new double[] { 22, 24 }, Products = new Product(){ ProductA = 15, ProductB = 18 } }
+                },
+
+                new ChartModel()
+                {
+                    Month = "Apr",
+                    Data = new Model() { YValue = new double[] { 16, 18 }, Products = new Product(){ ProductA = 8, ProductB = 14 } }
+                },
+
+                new ChartModel()
+                {
+                    Month = "May",
+                    Data = new Model() { YValue = new double[] { 32, 28 }, Products = new Product(){ ProductA = 22, ProductB = 28 } }
+                },
+
+                new ChartModel()
+                {
+                    Month = "Jun",
+                    Data = new Model() { YValue = new double[] { 30, 23 }, Products = new Product(){ ProductA = 18, ProductB = 24 } }
+                },
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void RaisePropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
